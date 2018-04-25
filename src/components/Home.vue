e16b501ae7802770ac4e0cee257d7e778f1d9ac8
alokpant <justtodesign@gmail.com>
2018-04-25 21:37:51 +0200

    Add files for Route and basic content

---
+++ src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container fluid>
+    <v-layout column>
+      <v-flex xs12 class="text-xs-center" mt-5>
+        Home Page
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
---
+++ src/components/Landing.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container fluid>
+    <v-layout column>
+      <v-flex xs12 class="text-xs-center" mt-5>
+        Landing page
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
