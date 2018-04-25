8fde95985d6cf8e61523c0973288b976eac8c179
alokpant <justtodesign@gmail.com>
2018-04-25 23:17:16 +0200

    Add navigation to the page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,39 @@
 <template>
   <v-app>
     <v-navigation-drawer v-model="sidebar" app>
+      <v-list>
+        <v-list-tile v-for='item in menuItems'
+                     :key='item.title'
+                     :to='item.path'>
+          <v-list-tile-action>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            {{item.title}}
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
     <v-toolbar app>
       <span class="hidden-sm-and-up">
         <v-toolbar-side-icon @click="sidebar = !sidebar">
         </v-toolbar-side-icon>
       </span>
-      <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
+      <v-toolbar-title>
+        <router-link to='/' tag='span' style='cursor: pointer'>
+          {{ appTitle }}
+        </router-link>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat
+               v-for='item in menuItems'
+               :key='item.title'
+               :to='item.path'>
+          <v-icon left dark>{{item.icon}}</v-icon>
+          {{item.content}}
+        </v-btn>
+      </v-toolbar-items>
     </v-toolbar>
 
     <v-content>
@@ -23,7 +48,12 @@ export default {
   data() {
     return {
       sidebar: false,
-      appTitle: 'Authentication with Firebase'
+      appTitle: 'Authentication with Firebase',
+      menuItems: [
+        { title: 'Home', path: '/home', icon: 'Home'},
+        { title: 'Sign Up', path: '/sign-up', icon: 'face'},
+        { title: 'Sign In', path: '/sign-in', icon: 'lock_open'}
+      ]
     }
   },
   name: 'App',
