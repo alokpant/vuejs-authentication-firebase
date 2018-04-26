353634d71caf3de128d66cf692d9b614fc4939b9
alokpant <justtodesign@gmail.com>
2018-04-26 05:24:43 +0200

    Add Homepage text

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,6 +4,13 @@
       <v-flex xs12 class="text-xs-center" mt-5>
         Home Page
       </v-flex>
+      <v-flex x12 sm6 offset-sm3 mt-3>
+        <p>This is a user's home page</p>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
+
+<script>
+  export default {};
+</script>
